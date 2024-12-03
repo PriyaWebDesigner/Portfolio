77b5417e160af24dcce0b13263a7c1661b6cd666
PriyaWebDesigner <naskartanu@111gmail.com>
2024-12-03 23:20:49 +0530

    Portfolio

---
+++ resources/views/frontend/includes/style.blade.php
@@ -0,0 +1,16 @@
+<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+<link rel="stylesheet" href="{{asset('frontend/assets/css/open-iconic-bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/animate.css')}}">
+
+<link rel="stylesheet" href="{{asset('frontend/assets/css/owl.carousel.min.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/owl.theme.default.min.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/magnific-popup.css')}}">
+
+<link rel="stylesheet" href="{{asset('frontend/assets/css/aos.css')}}">
+
+<link rel="stylesheet" href="{{asset('frontend/assets/css/ionicons.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('frontend/assets/css/flaticon.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/icomoon.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/assets/css/style.css')}}">
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -5,52 +5,12 @@
 	<title>Priya Naskar</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/open-iconic-bootstrap.min.css')}}">
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/animate.css')}}">
-
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/owl.carousel.min.css')}}">
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/owl.theme.default.min.css')}}">
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/magnific-popup.css')}}">
-
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/aos.css')}}">
-
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/ionicons.min.css')}}">
-
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/flaticon.css')}}">
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/icomoon.css')}}">
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/style.css')}}">
+    @include('frontend.includes.style')
 </head>
 
 <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
 
-
-	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar ftco-navbar-light site-navbar-target" id="ftco-navbar">
-		<div class="container">
-			<a class="navbar-brand" href="index.html">PRIYA</a>
-			<button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse"
-				data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="oi oi-menu"></span> Menu
-			</button>
-
-			<div class="collapse navbar-collapse" id="ftco-nav">
-				<ul class="navbar-nav nav ml-auto">
-					<li class="nav-item active"><a href="#home-section" class="nav-link"><span>Home</span></a></li>
-					<li class="nav-item"><a href="#about-section" class="nav-link"><span>About</span></a></li>
-					<li class="nav-item"><a href="#education-section" class="nav-link"><span>Education</span></a></li>
-					<!-- <li class="nav-item"><a href="#resume-section" class="nav-link"><span>Resume</span></a></li> -->
-					<li class="nav-item"><a href="#services-section" class="nav-link"><span>Services</span></a></li>
-					<li class="nav-item"><a href="#skills-section" class="nav-link"><span>Skills</span></a></li>
-					<li class="nav-item"><a href="#interest-section" class="nav-link"><span>Interest</span></a></li>
-					<!-- <li class="nav-item"><a href="#projects-section" class="nav-link"><span>Projects</span></a></li>
-					<li class="nav-item"><a href="#blog-section" class="nav-link"><span>My Blog</span></a></li> -->
-					<li class="nav-item"><a href="#contact-section" class="nav-link"><span>Contact</span></a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+	@include('frontend.includes.header')
 	<section id="home-section" class="hero">
 		<div class="home-slider  owl-carousel">
 			<div class="slider-item ">
