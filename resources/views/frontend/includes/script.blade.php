0cce73f67aea22cd5954d3d0070d5d72dda40105
PriyaWebDesigner <naskartanu@111gmail.com>
2024-12-04 08:28:06 +0530

    Portfolio

---
+++ resources/views/frontend/includes/script.blade.php
@@ -0,0 +1,14 @@
+<script src="{{asset('frontend/assets/js/jquery.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery-migrate-3.0.1.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/popper.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.easing.1.3.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.stellar.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/aos.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.animateNumber.min.js')}}"></script>
+<script src="{{asset('frontend/assets/js/scrollax.min.js')}}"></script>
+
+<script src="{{asset('frontend/assets/js/main.js')}}"></script>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -839,107 +839,7 @@
 			</div>
 		</div>
 	</section>
-
-
-	<footer class="ftco-footer ftco-section">
-		<div class="container">
-			<div class="row mb-5">
-				<div class="col-md-4">
-					<div class="ftco-footer-widget mb-4">
-						<h2 class="ftco-heading-2">Why Choose Me</h2>
-						<p>As a professional Web Designer and Developer, I offer expertise in creating stunning,
-							functional websites. I prioritize understanding your unique needs, ensuring every
-							project is crafted with creativity and strategic planning. I make your digital dreams a
-							reality with style, engagement, and smooth functionality. Let's collaborate to elevate your
-							online presence and achieve your web design goal.</p>
-						<!-- <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-							<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-							<li class="ftco-animate"><a href="https://www.facebook.com/" target="_blank"><span class="icon-facebook"></span></a></li>
-							<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-							<li class="ftco-animate"><a href="#"><span class="icon-linkedin"></span></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-md-2">
-					<div class="ftco-footer-widget mb-4 ml-md-4">
-						<h2 class="ftco-heading-2">Links</h2>
-						<ul class="list-unstyled">
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Home</a></li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>About</a></li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Education</a></li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Services</li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Skills</li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Interest</li>
-							<!-- <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Projects</a></li> -->
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Contact</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="ftco-footer-widget mb-4">
-						<h2 class="ftco-heading-2">Services</h2>
-						<ul class="list-unstyled">
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Web Design</a></li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Web Development</a></li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Satic Website</a></li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Dynamic Website</a></li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Web Application</a></li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>App Development</a></li>
-							<li><a href="#"><span class="icon-long-arrow-right mr-2"></span>SEO Advance</a></li>
-							<!-- <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Graphic Design</a></li> -->
-						</ul>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="ftco-footer-widget mb-4">
-						<h2 class="ftco-heading-2">Have a Questions?</h2>
-						<div class="block-23 mb-3">
-							<ul>
-								<li><span class="icon icon-map-marker"></span><a
-										href="https://maps.app.goo.gl/6JvqLN8ziJMhPstY8" target="_blank">Kolkata Leather
-										Complex, Kolkata, West Bengal, India</a></li>
-								<li><a href="#"><span class="icon icon-phone"></span><span class="text">+91
-											9832854019</span></a></li>
-								<li><a href="#"><span class="icon icon-envelope"></span><span
-											class="text">naskartanu111@gmail.com</span></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- <span class="text">Kolkata Leather Complex, Kolkata, West Bengal, India</span> -->
-			<div class="row">
-				<div class="col-md-12 text-left">
-					<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-						<li class="ftco-animate"><a href="#" target="_blank"><span class="icon-twitter"></span></a></li>
-						<li class="ftco-animate"><a href="https://www.facebook.com/profile.php?id=100077226802472"
-								target="_blank"><span class="icon-facebook"></span></a></li>
-						<li class="ftco-animate"><a href="https://www.instagram.com/freelancer_ranjit/"
-								target="_blank"><span class="icon-instagram"></span></a></li>
-						<li class="ftco-animate"><a href="https://www.linkedin.com/in/ranjit-biswas-219723219/"
-								target="_blank"><span class="icon-linkedin"></span></a></li>
-					</ul>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-md-12 text-center">
-
-					<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;
-						<script>document.write(new Date().getFullYear());</script> is designed by PRIYA. |
-						All Rights Reserved
-						<!-- is made with <i class="icon-heart color-danger" aria-hidden="true"></i> -->
-						<!-- <a href="https://colorlib.com" target="_blank">Colorlib</a> -->
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-					</p>
-				</div>
-			</div>
-		</div>
-	</footer>
-
-	<!-- Copyright ©2024 by JOBAYER. | All Rights Reserved -->
+	@include('frontend.includes.footer')
 
 	<!-- loader -->
 	<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
@@ -948,21 +848,7 @@
 				stroke="#F96D00" />
 		</svg></div>
 
-
-	<script src="{{asset('frontend/assets/js/jquery.min.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/jquery-migrate-3.0.1.min.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/popper.min.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/bootstrap.min.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/jquery.easing.1.3.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/jquery.waypoints.min.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/jquery.stellar.min.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/owl.carousel.min.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/jquery.magnific-popup.min.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/aos.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/jquery.animateNumber.min.js')}}"></script>
-	<script src="{{asset('frontend/assets/js/scrollax.min.js')}}"></script>
-
-	<script src="{{asset('frontend/assets/js/main.js')}}"></script>
+		@include('frontend.includes.script')
 
 </body>
 
