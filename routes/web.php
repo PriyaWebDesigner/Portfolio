b16f305743773bdd252a4b52f8dbfd5a1bbadecd
PriyaWebDesigner <naskartanu@111gmail.com>
2024-12-09 16:00:07 +0530

    POrtfolio

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\Frontend\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[FrontendController::class,'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
