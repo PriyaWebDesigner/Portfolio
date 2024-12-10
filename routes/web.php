fc84e8f3e7436d82134f61ba8b142b9ab0448a67
PriyaWebDesigner <naskartanu@111gmail.com>
2024-12-10 12:53:31 +0530

    Portfolio

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\AuthController;
 use App\Http\Controllers\Frontend\FrontendController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+//Frontend Url
 Route::get('/',[FrontendController::class,'index']);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Admin Login Url
+Route::get('/admin/login', [AuthController::class, 'adminLogin'])->name('adminLogin');
+
+//Admin Pannel
+Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('adminDashboard');
